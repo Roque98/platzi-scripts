a98bd44934b51708ac85531fea1eeaf0e47d8431
Roque98 <41236879+Roque98@users.noreply.github.com>
2020-08-11 18:51:19 -0500

    Update install.sh

--- install-jenkins/install.sh
+++ install-jenkins/install.sh
@@ -1,9 +1,10 @@
 # Depending on your system you might need to use sudo
 apt-get update
 apt-get install openjdk-8-jdk wget gnupg
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
-apt-get update
-apt-get install git jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install git jenkins
 ssh-keygen
 service jenkins start
